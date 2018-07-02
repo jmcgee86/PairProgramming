331e4bb2a27526f2b59afd0694a5bc9d3dceddfe
jbristol85 <josh.bristol2@gmail.com>
2018-07-02 17:43:27 -0400

    Initial commit

--- PairProgramming/Program.cs
+++ PairProgramming/Program.cs
@@ -11,7 +11,7 @@ namespace PairProgramming
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
+           
         }
     }
 }
